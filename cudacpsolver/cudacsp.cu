c3d12e42a48f890368d68a8375c5383d82ba6af9
李哲 <leezear@live.cn>
2017-11-30 15:54:59 +0800

    21

---
+++ cudacpsolver/cudacsp.cu
@@ -0,0 +1,5 @@
+#include "../include/HModel.h"
+
+float BuidBitModel32bit(cudacp::HModel * hm) {
+	return 0.0f;
+}
---
+++ test/kernel.cu
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <iostream>
+#include "../include/XCSP3PrintCallbacks.h"
+#include "../include/HModel.h"
+#include "../include/BMFileParser.h"
+
+using namespace cudacp;
+using namespace std;
+//#define LOGFILE
+
+const int TimeLimit = 600000;
+const string x_path = "BMPath.xml";
+
+int main() {
+	string bm_path;
+	if (FindBMPath(x_path))
+		bm_path = _bm_path;
+	cout << bm_path << endl;
+	HModel *hm = new HModel();
+	GetHModel(bm_path, hm);
+	return 0;
+
+}
